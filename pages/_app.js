c3333874a74469264b65f21dff3db3199f27ceb0
Wai Yee <wwaiyyee@gmail.com>
2025-12-10 00:32:13 +0800

    integrate font

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,27 @@
 import { SessionProvider } from "next-auth/react";
+import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "@/styles/globals.css";
 
-export default function App({ 
-  Component, 
-  pageProps: { session, ...pageProps } 
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const playfair = localFont({
+  src: "../public/fonts/PlayfairDisplay-VariableFont_wght.ttf",
+  variable: "--font-playfair",
+});
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps }
 }) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <main className={`${inter.variable} ${playfair.variable} font-sans`}>
+        <Component {...pageProps} />
+      </main>
     </SessionProvider>
   );
 }
